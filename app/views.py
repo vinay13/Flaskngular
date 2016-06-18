367ee2260eb61dfcb4969e5717ffcee106d588d6
vinay singh <aitvinay@gmail.com>
2016-06-18 19:38:19 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from app import app
 
 from .models import Blog
 #from flask.ext import restful
-
+from flask import abort
 #from app import api, db
 #from models import  Blog
 #from forms import UserCreateForm, SessionCreateForm, PostCreateForm
@@ -41,3 +41,13 @@ blogs = [
 @app.route('/blogs', methods=['GET'])
 def get_blogs():
     return jsonify({'blogs': blogs})
+    
+    
+
+
+@app.route('/blogs/<int:blog_id>', methods=['GET'])
+def get_blog(blog_id):
+    blog = [blog for blog in blogs if blog['id'] == blog_id]
+    if len(blog) == 0:
+        abort(404)
+    return jsonify({'blog': blog[0]})    
